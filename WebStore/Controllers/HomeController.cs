8cb303a09f4e936777821128f10510757bff2869
dmitry <dmitry@dtech-ural.ru>
2020-11-25 16:09:50 +0500

    Добавил вывод данных на странице Сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,18 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Service;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly TestDB _db;
+        public HomeController(TestDB db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Employees()
         {
-            return View();
+            return View(_db.Employees);
         }
     }
 }
