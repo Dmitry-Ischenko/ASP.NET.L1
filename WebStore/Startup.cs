9f216574631a1c89ec1d948ea55f6ac8f2c9bd36
dmitry <dmitry@dtech-ural.ru>
2020-11-28 09:29:45 +0500

    Добавил Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -17,7 +17,7 @@ namespace WebStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddSingleton<TestDB>();
         }
 
