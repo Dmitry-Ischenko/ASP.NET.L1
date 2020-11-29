44d97fe9841b246b6cd828822a304dff1071eaf6
dmitry <dmitry@dtech-ural.ru>
2020-11-29 16:04:32 +0500

    Запилил разметку html и метод удаления сохранения данных

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,18 +23,6 @@ namespace WebStore.Controllers
         {
             return View(_db.Employees);
         }
-        [HttpPost]
-        public IActionResult DeleteEmployee(int id)
-        {
-            var employees = _db.Employees.FirstOrDefault(item => item.Id == id);
-            if (employees is not null)
-            {
-                _db.Employees.Remove(employees);
-                return RedirectToAction(nameof(Index));
-            }                
-            else
-                return NotFound();
-        }
         public IActionResult Details(int id)
         {
             var employees = _db.Employees.FirstOrDefault(item => item.Id == id);
@@ -43,21 +32,39 @@ namespace WebStore.Controllers
                 return NotFound();
         }
         [HttpPost]
-        public IActionResult Details(int id,string FirstName,string LastName,int Age)
-        {                        
-            var employees = _db.Employees.FirstOrDefault(item => item.Id == id);
-            if (employees is not null)
+        public IActionResult Details(IFormCollection form)
+        {
+            if (form.TryGetValue("id",out var idStr))
             {
-                if (!employees.FirstName.Equals(FirstName))
-                    employees.FirstName = FirstName;
-                if (!employees.LastName.Equals(FirstName))
-                    employees.LastName = LastName;
-                if (employees.Age != Age)
-                    employees.Age = Age;
-                return View(employees);
-            }                
-            else
-                return NotFound();
+                if (Int32.TryParse(idStr,out int id))
+                {
+                    var employee = _db.Employees.FirstOrDefault(item => item.Id == id);
+                    if (employee is not null)
+                    {
+                        if (form.ContainsKey("save"))
+                        {
+                            form.TryGetValue("FirstName", out var FirstName);
+                            form.TryGetValue("LastName", out var LastName);
+                            form.TryGetValue("Age", out var AgeStr);
+                            Int32.TryParse(AgeStr, out var Age);
+                            if (!employee.FirstName.Equals(FirstName))
+                                employee.FirstName = FirstName;
+                            if (!employee.LastName.Equals(FirstName))
+                                employee.LastName = LastName;
+                            if (employee.Age != Age)
+                                employee.Age = Age;
+                            return RedirectToAction(nameof(Employees));
+                        }
+                        if (form.ContainsKey("del"))
+                        {
+                            _db.Employees.Remove(employee);
+                            return RedirectToAction(nameof(Employees));
+                        }
+                    }
+                }
+            }
+            
+            return NotFound();
         }
     }
 }
