cec205db0b361dcfd7b9a2dc39b61328b4bfafbd
dmitry <dmitry@dtech-ural.ru>
2020-12-03 11:12:26 +0500

    модель блогов

---
+++ WebStore/Models/BlogPost.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Дата создания поста (размещения)
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Рейтинг поста (лайки/дизы)
+        /// </summary>
+        public int Rating { get; set; }
+        /// <summary>
+        /// Изображение задника блога
+        /// </summary>
+        public string ImageBackground { get; set; }
+        /// <summary>
+        /// Изображение для предварительного просмотра
+        /// </summary>
+        public string PreviewImage { get; set; }
+        /// <summary>
+        /// Заголовок поста
+        /// </summary>
+        public string Header { get; set; }
+
+        /// <summary>
+        /// Тело поста с Html разметкой
+        /// </summary>
+        public string BodyHtml { get; set; }
+    }
+}
