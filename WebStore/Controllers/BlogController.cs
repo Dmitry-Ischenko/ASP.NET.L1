ff3cd9b8eccf133471f393c944acc8f34fc981aa
dmitry <dmitry@dtech-ural.ru>
2020-12-03 11:14:00 +0500

    Добавил отображение сингл блога и описал поведение в контроллере

--- WebStore/Controllers/BlogController.cs
+++ WebStore/Controllers/BlogController.cs
@@ -3,19 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly IBlogsData _db;
+        public BlogController(IBlogsData db) => _db = db;
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult BlogPost()
+        public IActionResult BlogPost(int id)
         {
-            return View();
+            var blog = _db.Get(id);
+            if (blog is not null) 
+                return View(_db.Get(id));
+            else
+                return RedirectToAction(nameof(Index));
         }
     }
 }
