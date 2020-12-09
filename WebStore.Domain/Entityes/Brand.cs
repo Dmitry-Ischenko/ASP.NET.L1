b28f5c6cc5e616c5ade7772834966501dd9b6e35
dmitry <dmitry@dtech-ural.ru>
2020-12-09 12:04:24 +0500

    Добавил билиотеку сущностий, и описал сущности

---
+++ WebStore.Domain/Entityes/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entityes.Base;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes
+{
+    class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
---
+++ WebStore.Domain/Entityes/Product.cs
@@ -0,0 +1,23 @@
+﻿using WebStore.Domain.Entityes.Base;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes
+{
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int SectionId { get; set; }
+
+        public int? BrandId { get; set; }
+
+        public int Discount { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+        
+    }
+}
