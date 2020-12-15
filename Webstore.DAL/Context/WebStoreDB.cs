5628ff1f183d44a2fed48464476c584c96091c4e
dmitry <dmitry@dtech-ural.ru>
2020-12-15 10:22:36 +0500

    Описал контекст базы данных

---
+++ Webstore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+
+namespace Webstore.DAL.Context
+{
+    class WebStoreDB: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public WebStoreDB(DbContextOptions<WebStoreDB> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+        }
+    }
+}
