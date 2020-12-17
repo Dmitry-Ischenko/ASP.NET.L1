4ab1f3a5971fa3e0fc2bfcaa3aed354430a681f7
dmitry <dmitry@dtech-ural.ru>
2020-12-17 13:53:32 +0500

    добавил пакет Microsoft.EntityFrameworkCore.Design

---
+++ WebStore/Data/WebStoreDbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webstore.DAL.Context;
+
+namespace WebStore.Data
+{
+    public class WebStoreDbInitializer
+    {
+        private readonly WebStoreDB _db;
+
+        public WebStoreDbInitializer(WebStoreDB db)
+        {
+            _db = db;
+        }
+        public void Initialize()
+        {
+
+        }
+    }
+}
