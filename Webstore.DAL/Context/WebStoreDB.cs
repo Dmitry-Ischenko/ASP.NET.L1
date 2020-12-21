08669ed22aabf6cc2bc97f29f06c470fdf07febd
dmitry <dmitry@dtech-ural.ru>
2020-12-21 13:58:05 +0500

    Добавил в билиотеку слоя доступа к данным пакет Microsoft.AspNetCore.Identity.EntityFrameworkCore и изменил контекст доступа к бд

--- Webstore.DAL/Context/WebStoreDB.cs
+++ Webstore.DAL/Context/WebStoreDB.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WebStore.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebStore.Domain.Entities.Identity;
 
 namespace Webstore.DAL.Context
 {
-    public class WebStoreDB: DbContext
+    public class WebStoreDB: IdentityDbContext<User, Role, string>
     {
         public DbSet<Product> Products { get; set; }
 
