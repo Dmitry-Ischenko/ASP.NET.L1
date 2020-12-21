21db450fe2d177e2bd74809c5ce3975807672a77
dmitry <dmitry@dtech-ural.ru>
2020-12-21 14:04:16 +0500

    миграции

--- Webstore.DAL/Migrations/20201221090326_identity.Designer.cs
+++ Webstore.DAL/Migrations/20201221090326_identity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Webstore.DAL.Context;
 
 namespace Webstore.DAL.Migrations
 {
     [DbContext(typeof(WebStoreDB))]
-    partial class WebStoreDBModelSnapshot : ModelSnapshot
+    [Migration("20201221090326_identity")]
+    partial class identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
