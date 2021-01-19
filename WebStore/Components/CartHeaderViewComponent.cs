adfc7a245851a83f381840ad109f3f9aac7159c1
dmitry <dmitry@dtech-ural.ru>
2021-01-19 13:39:46 +0500

    Правки стилей и отображения корзины в заголовке

---
+++ WebStore/Components/CartHeaderViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Components
+{
+    public class CartHeaderViewComponent : ViewComponent
+    {
+        private readonly ICartService _CartService;
+
+        public CartHeaderViewComponent(ICartService CartService)
+        {
+            _CartService = CartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_CartService.TransformFromCart());
+        }
+    }
+}
