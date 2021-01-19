195bf5678e1d4d154213c74fd15ca8b89f831e29
dmitry <dmitry@dtech-ural.ru>
2021-01-19 15:14:06 +0500

    Добавляем админку

---
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Areas.Controllers
+{
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
