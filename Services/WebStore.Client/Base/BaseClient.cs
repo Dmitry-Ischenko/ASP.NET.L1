d1fc1a908a90b691048d199b6b26335d7af08d45
dmitry <dmitry@dtech-ural.ru>
2021-01-21 11:27:17 +0500

    веб клиент

---
+++ Services/WebStore.Client/Base/BaseClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+namespace WebStore.Client.Base
+{
+    public abstract class BaseClient
+    {
+        protected string Address { get; }
+        protected HttpClient Http { get; }
+
+        protected BaseClient(IConfiguration Configuration, string ServiceAddress)
+        {
+            Address = ServiceAddress;
+            Http = new HttpClient
+            {
+                BaseAddress = new Uri(Configuration["WebApiURL"]),
+                DefaultRequestHeaders =
+                {
+                    Accept = { new MediaTypeWithQualityHeaderValue("application/json") }
+                }
+            };
+        }
+    }
+}
