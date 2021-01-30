3698f456addac99c8c13f13f7e0d7425e3529d70
dmitry <dmitry@dtech-ural.ru>
2021-01-30 17:06:20 +0500

    контроллер для товаров

---
+++ Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webstore.Interfaces.Services;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase, IProductData
+    {
+        private readonly IProductData _db;
+        private readonly ILogger<ProductsApiController> _Logger;
+
+        public ProductsApiController(IProductData db, ILogger<ProductsApiController> Logger)
+        {
+            _db = db;
+            _Logger = Logger;
+        }
+
+        [HttpGet("brands")]
+        public IEnumerable<Brand> GetBrands()
+        {
+            return _db.GetBrands();
+        }
+
+        [HttpGet("brands/{id}")]
+        public Brand GetBrandsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("products/{id}")]
+        public Product GetProductById(int id)
+        {
+            return _db.GetProductById(id);
+        }
+
+        
+        [HttpGet("products")]
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            return _db.GetProducts(Filter);
+        }
+
+        [HttpGet("categories")]
+        public IEnumerable<Category> GetСategories()
+        {
+            return _db.GetСategories();
+        }
+
+        [HttpGet("categories/{id}")]
+        public Category GetСategoriesById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
