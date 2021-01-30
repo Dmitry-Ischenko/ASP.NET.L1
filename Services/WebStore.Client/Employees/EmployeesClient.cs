938b543c37133e926e13291448d9635e6e579c3e
dmitry <dmitry@dtech-ural.ru>
2021-01-30 13:39:20 +0500

    Клиент для сотрудников

--- Services/WebStore.Client/Employees/EmployeesClient.cs
+++ Services/WebStore.Client/Employees/EmployeesClient.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using Webstore.Interfaces.Services;
 using WebStore.Client.Base;
@@ -11,36 +12,25 @@ namespace WebStore.Client.Employees
 {
     public class EmployeesClient: BaseClient, IEmployeesData
     {
-        private readonly ILogger<EmployeesClient> logger;
+        private readonly ILogger<EmployeesClient> _Logger;
 
         public EmployeesClient(IConfiguration Configuration, ILogger<EmployeesClient> Logger): base(Configuration, "api/employees")
         {
-            logger = Logger;
+            _Logger = Logger;
         }
 
-        public int Add(Employee employee)
-        {
-            throw new NotImplementedException();
-        }
+        public int Add(Employee employee)=>Post(Address,employee).Content.ReadAsAsync<int>().Result;
 
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
 
-        public IEnumerable<Employee> Get()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
 
-        public Employee Get(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
 
         public void Update(Employee employee)
         {
-            throw new NotImplementedException();
+            _Logger.LogInformation("Редактирование сотрудника с id:{ 0}", employee.Id);
+            Put(Address, employee);
         }
     }
 }
